8b7ea27e94f98700cb1b3346f64826b02c04b5b5
FarhadIbrahimov <farhadibrahimov@gmail.com>
2023-08-22 21:59:19 -0600

    git.put practice added

--- app.js
+++ app.js
@@ -34,19 +34,32 @@ app.post("/api/postman/people", (req, res) => {
   res.status(201).json({ success: true, data: [...people, name] });
 });
 
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `there is no person with id ${id}` });
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+});
+
 app.post("/login", (req, res) => {
   const { name } = req.body;
   if (name) {
     return res.status(200).send(`Welcome ${name}`);
   }
   res.status(401).send("Please provide Credentials");
-});
-
-app.put("/api/people/:id", (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-  console.log(req.params);
-  res.send("Bingo!");
 });
 
 app.listen(5000, () => {
